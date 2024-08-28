82e1e5d59a9c73ba2ff028f55815a78b2aedf6fb
Allen Liang <notallenliang293@gmail.com>
2024-08-28 09:58:51 +0800

    Lab is Complete

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner=new Scanner(System.in);				// line 1
+        System.out.println("Enter your age:");		// line 2
+        int age=-1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number!");
+            }
+        }
+        int dateable=(age/2)+7;// line 3
+        System.out.println(age+"-year olds should date somebody who is at least "+dateable+" years old.");
     }
 }
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");		// line 2
+        int age=-1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number!");
+            }
+        }
+        int page=100-age;
+        System.out.print(age + "-year olds should read at least " + page + " pages before giving up on a book.");
     }
 }
